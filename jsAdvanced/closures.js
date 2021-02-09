03d2e76c8842ff0ce281f01ec1644573337f1b8f
madan-dinata <madanmr48@gmail.com>
2021-02-09 07:16:02 +0700

    add closures

--- jsAdvanced/closures.js
+++ jsAdvanced/closures.js
@@ -1,11 +1,40 @@
-// Closure merupakan kombinasi antara function dan lingkungan lexical di dalam fuction tersebut
+// Closure merupakan kombinasi antara function dan lingkungan lexical di dalam fuction tersebut.
+// Closure adalah function yang mengakses / menggunakan variabel dari function parent nya
 
-function coba() {
-    let nama = "madan"
-    function tampilCoba() {
-        console.log(nama)
-    }
-    tampilCoba()
-}
+// function coba() {
+//     let age = 20
+//     return function(name) { 
+//         console.dir(`Halo, nama saya ${name}, umur ${age} tahun`)
+//         //ini adalah closure karna menggambil variabel age di atas parent scope nya, yaitu di function coba()
+//     }
+// }
 
-coba()
+// let panggil = coba()
+// panggil("madan", 20)
+
+
+
+// function salam(time) {
+//     return function(name) {
+//         console.log(`Halo ${name}, Selamat ${time}`)
+//         // Ini adalah closure karna menggunakan variabel time dari  function salam()
+//     }
+// }
+
+// let selamatPagi = salam("Pagi")
+// let selamatSiang = salam("Siang")
+// let selamatMalam = salam("Malam")
+
+// selamatPagi("Madan")
+
+
+
+const add = (() => {
+    let counter = 0
+    return () => ++counter
+})()
+
+console.log(add())
+console.log(add())
+console.log(add())
+console.log(add())
